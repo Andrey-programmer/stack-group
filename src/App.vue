8aed4f82d6bf482eeea9e2a012b96ffa4451aa79
Andrey_programmer <lak0sta1302@gmail.com>
2019-06-20 20:15:53 +0300

    Первый коммит

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar 
+      app 
+      class="text-xs-center"
+    >
+      <v-toolbar-title class="headline">
+        <span>Меню</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn 
+        v-for="item of menu"
+        :key="item.name"
+        :to="item.link"
+        active-class="v-btn--active"
+        flat
+      >
+        {{item.name}}
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      menu: [
+        {
+          name: "Пользователи",
+          link: '/users'
+        },
+        {
+          name: "Группы",
+          link: '/groups'
+        }
+      ]
     }
   }
 }
-</style>
+</script>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Users from './components/Users'
+import Groups from './components/Groups'
 
 Vue.use(Router)
 
@@ -11,15 +12,22 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      redirect: '/users'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/users',
+      name: 'users',
+      component: Users
+    },
+    {
+      path: '/groups',
+      name: 'groups',
+      component: Groups
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
